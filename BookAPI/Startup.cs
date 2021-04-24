befcf22981eda7bfe7c41d51f74e206958cdd2e9
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-24 19:38:25 +0530

    Update ConfigureServices in startup.cs - Register BookContext for dependency injection

--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -1,5 +1,7 @@
+using BookAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace BookAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("BookContext")));
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "BookAPI", Version = "v1"}); });
         }
