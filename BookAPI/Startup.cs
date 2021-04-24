a70da937631e7ce89aadf46f44d59ef9faa823a7
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-24 21:36:17 +0530

    Register BookRepository with dependency injection

--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -1,4 +1,5 @@
 using BookAPI.Models;
+using BookAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,7 @@ namespace BookAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddDbContext<BookContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("BookContext")));
             services.AddControllers();
