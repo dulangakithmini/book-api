6a59935dcdf6a57b163924e4c131a1c4c332c01c
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-24 21:58:11 +0530

    Create BooksController

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
