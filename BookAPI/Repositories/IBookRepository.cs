9cd3ce8b89298d22f73e77f2392ac10d69cafa78
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-24 20:08:42 +0530

    Create BookRepository Interface

---
+++ BookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookAPI.Models;
+
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task<Book> Update(Book book);
+
+        Task<Book> Delete(int id);
+    }
+}
